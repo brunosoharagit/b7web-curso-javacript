1adeaed02db05c74d99a8e10dd4f221c6f8f43cc
brunosoharagit <b.sohara.git@gmail.com>
2021-10-27 18:15:11 -0300

    B7WEBJS 2021.10.27

--- modulo01/script.js
+++ modulo01/script.js
@@ -1,1 +1,24 @@
-alert("Olá mundo!!");
+//alert("Olá mundo!!");
+
+//var inteiro = 10;
+
+//var nome = 'Bruno';
+//var sobrenome = 'Sohara';
+
+//var nomeesobrenome = nome + ' ' + sobrenome;
+//console.log(nomeesobrenome);
+
+//console.log(inteiro);
+//alert(inteiro)
+
+var hora = 20;
+
+if ((hora > 0) && (hora < 12)) {
+    alert("Bom dia!");
+}
+else if ((hora >= 12) && (hora < 17)){
+    alert("Boa tarde!");
+}
+else {
+    alert("Boa noite!");
+}
