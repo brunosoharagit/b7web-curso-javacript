1c2ba747b272def3f518a18b6a483b32deafce1b
brunosoharagit <b.sohara.git@gmail.com>
2021-11-01 09:38:50 -0300

    B7WEBJS 2021.11.01 - alteração 3

--- modulo01/script.js
+++ modulo01/script.js
@@ -3,12 +3,19 @@ alert("Olá mundo!!");
 //tipos de variáveis
 //a. var
 var nome = "Bruno";
+var nome = "Ulisses";
 
 //b. let
-let nome = "Bruno";
+//let nome = "Bruno";
 
 //c. const
-const nome = "Bruno";
+//const nome = "Bruno";
+
+if (nome == "Bruno"){
+    var idade = 90;
+}
+
+console.log(idade);
 
 
 
